dfe8c0261060563cb03459a5e6f8d768ccdd8d88
Ihor <ihorfront88@gmail.com>
2022-08-25 18:53:11 +0300

    add axios

--- src/js/services/imagesApi.js
+++ src/js/services/imagesApi.js
@@ -1,9 +1,14 @@
-const BASE_URL = 'https://pixabay.com/api';
+import axios from 'axios';
+
 const API_KEY = '25802265-f217fc33d7f2a9a9b0a0a0132';
 
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
 const fetchImages = (inputValue, perPage, page) => {
-  return fetch(
-    `${BASE_URL}/?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`
-  ).then(res => (res.ok ? res.json() : Promise.reject(res.statusText)));
+  return axios
+    .get(
+      `/?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`
+    )
+    .then(res => res.data);
 };
 export { fetchImages };
